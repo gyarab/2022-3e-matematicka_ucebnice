7cefd95b2ad95c7598b63b4b6d90cfe7d3d7644c
jakub.sveda <jakub.sveda@icloud.com>
2022-10-18 10:44:25 +0200

    initial structure

---
+++ mathematic-edu-tool/pages/index.jsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+
+function Home() {
+  return (
+    <div>
+      <Head>
+          <title>Create Next App</title>
+          <meta name="description" content="" /> {/* TODO -> description */}
+      </Head>
+
+      <main>
+
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
+
+export default Home;
