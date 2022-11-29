3473069045db7a0c5ef7f8886f016fb40858b80d
jakub.sveda <jakub.sveda@icloud.com>
2022-11-29 21:07:39 +0100

    Bootstrap navbar created; docs

--- components/nav-bar/NavBar.jsx
+++ components/nav-bar/NavBar.jsx
@@ -1,25 +1,53 @@
-import Link from 'next/link'
-import navbarStyles from '../../styles/NavBar.module.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 /**
- * Navigator
+ * NAVIGATOR
+ *
  * - bar on the top of the page
  * - after clicking on one of the buttons is user redirected to the requested route
  *
- * @param props
+ * @param currentRoute
  * @returns {JSX.Element}
  * @constructor
  */
-const NavBar = () => {
+const NavBar = ({currentRoute}) => {
+    /*
+    TODO -> implement marking current route
+    TODO -> redesign colors of this navbar (rewrite bootstrap variables)
+     */
+
     return (
-        <nav>
-            <Link href={'/homepage'}>Home</Link>
-            <Link href={'/class/sixth'}>Šestá třída</Link>
-            <Link href={'/class/seventh'}>Sedmá třída</Link>
-            <Link href={'/class/eight'}>Osmá třída</Link>
-            <Link href={'/class/ninth'}>Devátá třída</Link>
-        </nav>
+        <Navbar bg={"--bs-blue"} expand={"lg"}>
+            <Container>
+                <Navbar.Brand href={"/homepage"}>Matematická učebnice</Navbar.Brand>
+                <Navbar.Toggle aria-controls={"basic-navbar-nav"}/>
+                <Navbar.Collapse id={"basic-navbar-nav"}>
+                    <Nav className={"me-auto"}>
+                        <Nav.Link href={"/class/sixth"}>Šestá třída</Nav.Link>
+                        <Nav.Link href={"/class/seventh"}>Sedmá třída</Nav.Link>
+                        <Nav.Link href={"/class/eight"}>Osmá třída</Nav.Link>
+                        <Nav.Link href={"/class/ninth"}>Devátá třída</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
+
+    /*
+    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">
+            Another action
+        </NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider/>
+        <NavDropdown.Item href="#action/3.4">
+            Separated link
+        </NavDropdown.Item>
+    </NavDropdown>
+     */
 }
 
 export default NavBar
