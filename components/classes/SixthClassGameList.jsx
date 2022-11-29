324eb70bd1f55bda35a01bb2ab85199e52e85375
jakub.sveda <jakub.sveda@icloud.com>
2022-11-29 21:08:23 +0100

    sixth class game list created; might not be used later

---
+++ components/classes/SixthClassGameList.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+const SixthClassGameList = () => {
+    /*
+    TODO -> is good to use external universal component?
+     */
+
+    return (
+        <div className={'container-fluid'}>
+            <ul>
+                <li>
+                    <Link href={}>
+
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default SixthClassGameList
