ed5ada3cdc3954740bc21dbc8311f761f3d4d33c
jakub.sveda <jakub.sveda@icloud.com>
2022-11-29 08:58:27 +0100

    footer docs added

--- components/utils/CustomFooter.jsx
+++ components/utils/CustomFooter.jsx
@@ -1,3 +1,12 @@
+/**
+ * Custom footer element
+ * - component creates footer of an application
+ * - should be used on every route of the project except API routes
+ *
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const CustomFooter = (props) => {
     return (
         <footer>
