50f209141edf7df5c7b878df14171c3c2a440221
jakub.sveda <jakub.sveda@icloud.com>
2022-12-02 10:25:08 +0100

    first game implementation

--- pages/class/eight.jsx
+++ pages/class/eight.jsx
@@ -2,6 +2,9 @@ import NavBar from "../../components/nav-bar/NavBar";
 import CustomHead from "../../components/utils/CustomHead";
 import {colorThemeDark, colorThemeLight} from "../../lib/env-variables";
 import CustomFooter from "../../components/utils/CustomFooter";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ChooseCorrectAnswer from "../../components/games/ChooseCorrectAnswer";
+
 
 const EightClass = (props) => {
     /*
@@ -17,9 +20,14 @@ const EightClass = (props) => {
                 themeColorLight={colorThemeLight}
                 themeColorDark={colorThemeDark}
             />
-            <main className={'container-fluid'}>
+            <main>
                 <NavBar />
-                <h1>osmá třída</h1>
+                <div className={'container-fluid'}>
+                    <h1>osmá třída</h1>
+                    <ChooseCorrectAnswer
+                        question={'Kolik je 1+1?'} answers={['1', '3', '6', '2', '0', '10', '30']} correctAnswer={'2'} helperText={'Podívej se na internet!!!'} equation={null}
+                    />
+                </div>
             </main>
             <CustomFooter/>
         </>
