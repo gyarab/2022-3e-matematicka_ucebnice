f5d9e16ddcafd41adf335cb4dd806e3857739a25
jakub.sveda <jakub.sveda@icloud.com>
2022-12-03 14:35:31 +0100

    navigator panels design improvement

--- pages/homepage.jsx
+++ pages/homepage.jsx
@@ -17,7 +17,7 @@ import {useRouter} from "next/router";
  */
 const HomePage = (props) => {
     /*
-    TODO -> create panels as links to classes
+    TODO -> add panels' description
     TODO -> how to use this app?
      */
 
@@ -32,79 +32,78 @@ const HomePage = (props) => {
             />
 
             <main>
-                <NavBar/>
-                <div className={'container-fluid m-3'}>
-                    <Container>
-                        <Row className={'container-fluid justify-content-center align-items-start'}>
-                            <Card
-                                bg={'primary'}
-                                text={'white'}
-                                style={{width: '18rem'}}
-                                className="m-4 mb-2"
-                            >
-                                <Card.Body>
-                                    <Card.Title>Šestá třída</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant={'secondary'}
-                                            onClick={() => router.push('/class/sixth')}>Vstoupit do třídy</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card
-                                bg={'primary'}
-                                text={'white'}
-                                style={{width: '18rem'}}
-                                className="m-4 mb-2"
-                            >
-                                <Card.Body>
-                                    <Card.Title>Sedmá třída</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant={'secondary'}
-                                            onClick={() => router.push('/class/seventh')}>Vstoupit do třídy</Button>
-                                </Card.Body>
-                            </Card>
-                        </Row>
-                        <Row className={'container-fluid justify-content-center'}>
-                            <Card
-                                bg={'primary'}
-                                text={'white'}
-                                style={{width: '18rem'}}
-                                className="m-4 mb-2"
-                            >
-                                <Card.Body>
-                                    <Card.Title>Osmá třída</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant={'secondary'}
-                                            onClick={() => router.push('/class/eight')}>Vstoupit do třídy</Button>
-                                </Card.Body>
-                            </Card>
-                            <Card
-                                bg={'primary'}
-                                text={'white'}
-                                style={{width: '18rem'}}
-                                className="m-4 mb-2"
-                            >
-                                <Card.Body>
-                                    <Card.Title>Devátá třída</Card.Title>
-                                    <Card.Text>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant={'secondary'}
-                                            onClick={() => router.push('/class/ninth')}>Vstoupit do třídy</Button>
-                                </Card.Body>
-                            </Card>
-                        </Row>
-                    </Container>
+                <NavBar
+                    imgPath={'/android-chrome-512x512.png'}
+                />
+                <div className={'container-fluid'}>
+                    
                 </div>
+                <Container>
+                    <Row className={'container-fluid justify-content-center align-items-start'}>
+                        <Card
+                            bg={'primary'}
+                            text={'white'}
+                            style={{width: '18rem'}}
+                            className="m-4 mb-2"
+                        >
+                            <Card.Body>
+                                <Card.Title>Šestá třída</Card.Title>
+                                <Card.Text>
+                                    Description
+                                </Card.Text>
+                                <Button variant={'secondary'}
+                                        onClick={() => router.push('/class/sixth')}>Vstoupit do třídy</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card
+                            bg={'primary'}
+                            text={'white'}
+                            style={{width: '18rem'}}
+                            className="m-4 mb-2"
+                        >
+                            <Card.Body>
+                                <Card.Title>Sedmá třída</Card.Title>
+                                <Card.Text>
+                                    Description
+                                </Card.Text>
+                                <Button variant={'secondary'}
+                                        onClick={() => router.push('/class/seventh')}>Vstoupit do třídy</Button>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                    <Row className={'container-fluid justify-content-center'}>
+                        <Card
+                            bg={'primary'}
+                            text={'white'}
+                            style={{width: '18rem'}}
+                            className="m-4 mb-2"
+                        >
+                            <Card.Body>
+                                <Card.Title>Osmá třída</Card.Title>
+                                <Card.Text>
+                                    Description
+                                </Card.Text>
+                                <Button variant={'secondary'}
+                                        onClick={() => router.push('/class/eight')}>Vstoupit do třídy</Button>
+                            </Card.Body>
+                        </Card>
+                        <Card
+                            bg={'primary'}
+                            text={'white'}
+                            style={{width: '18rem'}}
+                            className="m-4 mb-2"
+                        >
+                            <Card.Body>
+                                <Card.Title>Devátá třída</Card.Title>
+                                <Card.Text>
+                                    Description
+                                </Card.Text>
+                                <Button variant={'secondary'}
+                                        onClick={() => router.push('/class/ninth')}>Vstoupit do třídy</Button>
+                            </Card.Body>
+                        </Card>
+                    </Row>
+                </Container>
             </main>
             <CustomFooter/>
         </>
