04b56f1e220b43eba4cd283c2f32920b18ee7f96
jakub.sveda <jakub.sveda@icloud.com>
2022-12-04 17:21:41 +0100

    index.jsx page created and designed

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
 import CustomHead from "../components/utils/CustomHead";
-import {colorThemeLight, colorThemeDark} from "../lib/env-variables";
+import {colorThemeDark, colorThemeLight} from "../lib/env-variables";
 import CustomFooter from "../components/utils/CustomFooter";
+import LoginForm from "../components/verification/Login";
+import indexStyles from '../styles/IndexPage.module.css'
 
 /*
 async function getServerSideProps(context) {
@@ -45,8 +47,26 @@ const Home = () => {
                 themeColorDark={colorThemeDark}
             />
 
-            <main className={'container-fluid'}>
-
+            <main className={indexStyles.mainContainer}>
+                <div className={indexStyles.backgroundCircle}></div>
+                <div className={indexStyles.container}>
+                    <div className={indexStyles.contentContainer}>
+                        <h1 className={indexStyles.title}>Matematická učebnice</h1>
+                        <p className={indexStyles.text}>
+                            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Integer tempor. Duis pulvinar.
+                            Pellentesque arcu. Nulla est. Aliquam erat volutpat. Phasellus faucibus molestie nisl. In
+                            dapibus augue non sapien. Nulla quis diam. Sed ut perspiciatis unde omnis iste natus error
+                            sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                            inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Duis sapien nunc,
+                            commodo et, interdum suscipit, sollicitudin et, dolor. Duis risus. Pellentesque habitant
+                            morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec quis nibh at
+                            felis congue commodo. Mauris tincidunt sem sed arcu.
+                        </p>
+                    </div>
+                    <div className={indexStyles.contentContainer}>
+                        <LoginForm/>
+                    </div>
+                </div>
             </main>
 
             <CustomFooter/>
