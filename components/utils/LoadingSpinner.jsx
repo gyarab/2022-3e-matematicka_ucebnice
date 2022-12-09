81f63c98f4bff1ea514248e784d361220b8f37b3
jakub.sveda <jakub.sveda@icloud.com>
2022-12-09 16:42:13 +0100

    LoadingSpinner.jsx design and function update

--- components/utils/LoadingSpinner.jsx
+++ components/utils/LoadingSpinner.jsx
@@ -1,7 +1,42 @@
 import {Spinner} from "react-bootstrap";
+import loaderStyles from '../../styles/Loader.module.css'
+import {useEffect, useState} from "react";
 
 const LoadingSpinner = () => {
-    return <Spinner animation="border" variant="primary" />
+    const [dotNumber, setDotNumber] = useState(1)
+
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setDotNumber(prevState => (prevState + 1) % 4)
+        }, 250)
+
+        console.log(dotNumber)
+        return () => timeout
+    }, [dotNumber])
+
+    const renderDots = () => {
+        let dots = ''
+
+        for (let i = 0; i < dotNumber; i++) {
+            dots += '.'
+        }
+
+        return dots
+    }
+
+    return (
+        <div className={`${loaderStyles.container} mb-3`}>
+            <div className={`${loaderStyles.innerContainer} p-4`}>
+                <div>
+                    Loading
+                    {
+                        renderDots()
+                    }
+                </div>
+                <Spinner animation={"border"} variant={"primary"} className={'mt-3'}/>
+            </div>
+        </div>
+    )
 }
 
 export default LoadingSpinner
