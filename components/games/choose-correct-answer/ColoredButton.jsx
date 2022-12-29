d940ab4c25d55f7c55c6b2f3e58875986c7bd140
jakub.sveda <jakub.sveda@icloud.com>
2022-12-29 12:35:22 +0100

    unnecessary component deleted

--- components/games/choose-correct-answer/ColoredButton.jsx
+++
@@ -1,9 +0,0 @@
-import {Button} from "react-bootstrap";
-
-const ColoredButton = ({answer}) => {
-    return <Button variant={"outline-secondary"}
-                   className={`m-2`}
-                   onClick={(e) => console.log('clicked', e)}>{answer}</Button>
-}
-
-export default ColoredButton
