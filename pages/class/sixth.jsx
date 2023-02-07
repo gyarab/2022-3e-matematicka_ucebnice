ff750183ed040e0d290d93ac4dfdd5ea04c27577
jakub.sveda <jakub.sveda@icloud.com>
2023-02-07 15:51:44 +0100

    sixth.jsx class game update

--- pages/class/sixth.jsx
+++ pages/class/sixth.jsx
@@ -38,40 +38,6 @@ const sections = [
                 ]
             },
             {
-                id: 1,
-                title: 'ChooseCorrectAnswer.jsx',
-                content: [
-                    {
-                        gameId: 0,
-                        autogenerate: false,
-                        question: 'Kolik je 1+1?',
-                        answers: ['2', '3', '8'],
-                        correctAnswer: '2',
-                        helperText: 'Pokud budu mít jedno jablko a přidám ještě jedno, kolik jich budu mít?',
-                        equation: null,
-                    },
-                    {
-                        gameId: 1,
-                        autogenerate: false,
-                        question: '',
-                        answers: ['', ''],
-                        correctAnswer: '',
-                        helperText: '', // if not stated -> null
-                        equation: '', // if not stated -> null
-                    },
-                    {
-                        gameId: 2,
-                        autogenerate: true,
-                        class: '6',
-                    },
-                    {
-                        gameId: 3,
-                        autogenerate: true,
-                        class: '', // class number (required)
-                    }
-                ]
-            },
-            {
                 id: 2,
                 title: 'Pexeso.jsx',
                 size: 4, // should not be even
@@ -103,8 +69,6 @@ const sections = [
         games: [
             {
                 id: 0,
-                title: '',
-                content: []
             }
         ]
     }
