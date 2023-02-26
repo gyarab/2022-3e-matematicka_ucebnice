dee2e43c4757a89b2bbe44ad1107ce9f1e2c37cd
NellaCarraro <nella.carraro@student.gyarab.cz>
2023-02-27 00:08:42 +0100

    error stranka design

--- components/utils/Error.jsx
+++ components/utils/Error.jsx
@@ -1,14 +1,8 @@
 import Router from "next/router";
-import {Button} from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import verificationStyles from "../../styles/Verification.module.css";
 
-const Error = ({message, status}) => {
-
-    /*
-    TODO -> je potřeba nadesignovat pomocí bootstrapu
-    TODO -> např. <div classname={`d-flex p-2 align-items-center`}>  ===> je to jen random styl (jen abyste věděli, jak to vypadá)
-    TODO -> kdybyste nevěděli, pište...
-     */
+const Error = ({ message, status }) => {
 
     const generateButtonDescription = () => {
         if (status === 401)
@@ -28,24 +22,31 @@ const Error = ({message, status}) => {
     }
 
     return (
-        <div>
-            <div>
-                <div>
-                    <h1>{status}</h1><h1>CHYBA</h1>
-                </div>
-                <div>
-                    <p>
-                        {message}
-                    </p>
-                    <Button
-                        variant={"primary"}
-                        type={'submit'}
-                        className={`${verificationStyles.submitButton} m-1 hoverDarkShadow`}
-                        onClick={handleButtonClick}
-                    >
-                        {generateButtonDescription()}
-                    </Button>
-                </div>
+        <div className="container mx-auto pt-5">
+            <div className="">
+                <Card
+                    bg={'primary'}
+                    text={'white'}
+                    className={"mx-auto"}
+                    style={{ height: '15rem' }}
+                > <div className="container">
+                        <h1 className="pt-3 text-secondary">CHYBA {status}</h1>
+                        <p className="pt-3">
+                            {message}
+                        </p>
+                    </div>
+                    <div className="position-absolute bottom-0 start-20 pb-2 ps-2">
+                        <Button
+                            variant={"secondary"}
+                            type={'submit'}
+                            style={{ width: '8rem' }}
+                            className={`${verificationStyles.submitButton} m-1 hoverDarkShadow`}
+                            onClick={handleButtonClick}
+                        >
+                            {generateButtonDescription()}
+                        </Button>
+                    </div>
+                </Card>
             </div>
         </div>
     )
