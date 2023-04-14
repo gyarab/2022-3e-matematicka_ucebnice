1c5c967281b225bbec89906a3d78a79b8f6ae98f
egor <egor.chekunov@student.gyarab.cz>
2023-04-14 12:46:02 +0200

    yes

--- components/games/geometry/Geometry.jsx
+++ components/games/geometry/Geometry.jsx
@@ -1,9 +1,27 @@
-const Geometry = () => {
-    return (
-        <div>
+import { BlurFilter } from 'pixi.js';
+import { Stage, Container, Sprite, Text } from '@pixi/react';
+import { useMemo } from 'react';
 
-        </div>
-    )
+const Geometry = () => {
+
+
+  const blurFilter = useMemo(() => new BlurFilter(4), []);
+
+  return (
+    <Stage>
+      <Sprite
+        image="https://pixijs.io/pixi-react/img/bunny.png"
+        x={400}
+        y={270}
+        anchor={{ x: 0.5, y: 0.5 }}
+      />
+
+      <Container x={400} y={330}>
+        <Text text="Hello World" anchor={{ x: 0.5, y: 0.5 }} filters={[blurFilter]} />
+      </Container>
+    </Stage>
+  );
+
 }
 
 export default Geometry
