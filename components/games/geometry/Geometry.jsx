f7885f7ec74b918d7aec5821f31539d516e5855d
egor <egor.chekunov@student.gyarab.cz>
2023-04-14 14:57:07 +0200

    start geometry ☺

--- components/games/geometry/Geometry.jsx
+++ components/games/geometry/Geometry.jsx
@@ -1,7 +1,10 @@
-import {useCallback, useState} from "react"
+import {useCallback, useState, useMemo} from "react"
 import Button from 'react-bootstrap/Button';
 import {Stage,Graphics,Sprite,Container,useTick,color } from "@inlet/react-pixi";
-
+import gameStyles from "../../../styles/games/Game.module.css";
+import gameUtilsStyles from '../../../styles/games/GameUtils.module.css'
+import GameNav from "../GameNav";
+import { generateTriangleQuestion } from "../../../lib/geometryGeneration";
 import { Color } from 'pixi.js';
 class Dot {
   constructor(x, y) {
@@ -17,8 +20,7 @@ class Dot {
  * @param {number} tSettings 1 - right angle triangle, 2 - rovnostrany
  * @returns
  */
-
-const Geometry = ({question ="empty",shape =-1,value =-1,tSettings = -1}) => {
+const Geometry = (difficulty) => {
   //TODO functions, checking rightness, remove upper part of canvas, make better dotAdd using rewritting
   //parameters
   /*
@@ -33,8 +35,10 @@ const Geometry = ({question ="empty",shape =-1,value =-1,tSettings = -1}) => {
 
   */
 
-  //variables
+  //const [game, setGame] = useState(0)
+  const game = generateTriangleQuestion(1)
   const [dotList, setDotList]  = useState([])
+  const [stage, setStage] = useState(0);
   let bgColor = new Color(0x3C486B)
   let gridColor = new Color(0xF0F0F0)
   let dotColor = new Color(0xF9D949)
@@ -42,18 +46,47 @@ const Geometry = ({question ="empty",shape =-1,value =-1,tSettings = -1}) => {
 
   let dotLimit = 100
   
+  function generateGameSetting(){
+    
+  }
+  const handleNextStage = () => {
+    setStage(prevState => (prevState + 1))
+}
+
+const handlePreviousStage = () => {
+    
+    setStage(prevState => (prevState - 1))
+
+}
+/*
+setGame(prevState =>{
+  prevState = generateTriangleQuestion(1)
+  return[...prevState]
+})
+*/
+
+  const deleteLastDot = () =>{
+    setDotList(prevState =>{
+      let newList = []
+      for (let i = 0; i < prevState.length-1; i++){
+        newList.push(prevState.at(i))
+      }
+      return[...newList]
+    })
+  }
+
   const drawGrid = useCallback(g => {
 
     function lines_across(g1 = g) {
       for(let i = 0;i <10;i++){
-        g.moveTo(0,200+i*40)
-        g.lineTo(400, 200+i*40)
+        g.moveTo(0,i*40)
+        g.lineTo(400,i*40)
       }
     }
     function lines_down(g1 = g){
       for(let i = 0;i <10;i++){
-        g.moveTo(i*40,200)
-        g.lineTo(i*40,600)
+        g.moveTo(i*40,0)
+        g.lineTo(i*40,400)
       }
     }
     function hitBox(g1 = g) {
@@ -61,7 +94,7 @@ const Geometry = ({question ="empty",shape =-1,value =-1,tSettings = -1}) => {
         for(let j = 0;j <9;j++){
           g.beginFill(bgColor)
           g.lineStyle(0, bgColor, 0)
-          g.drawRect(25+i*40,225+j*40,30,30) 
+          g.drawRect(25+i*40,25+j*40,30,30) 
         }
       }
     }
@@ -83,19 +116,19 @@ const Geometry = ({question ="empty",shape =-1,value =-1,tSettings = -1}) => {
     for (let i = 0; i< dotList.length;i++){
       g.lineStyle(4, dotColor, 1)
       g.beginFill(lineColor)
-      g.drawCircle(dotList.at(i).x*40+40,dotList.at(i).y*40+240,8)
+      g.drawCircle(dotList.at(i).x*40+40,dotList.at(i).y*40+40,8)
     }
     //lines
     for (let i = 0; i< dotList.length-1;i++){
       g.lineStyle(8, lineColor, 1)
       g.beginFill(lineColor)
-      g.moveTo(dotList.at(i).x *40+40,dotList.at(i).y *40+240)
-      g.lineTo(dotList.at(i+1).x *40+40,dotList.at(i+1).y *40+240)
+      g.moveTo(dotList.at(i).x *40+40,dotList.at(i).y *40+40)
+      g.lineTo(dotList.at(i+1).x *40+40,dotList.at(i+1).y *40+40)
     }
     //last
     if(dotList.length>2){
-    g.moveTo(dotList.at(0).x *40+40,dotList.at(0).y *40+240)
-    g.lineTo(dotList.at(dotList.length-1).x *40+40,dotList.at(dotList.length-1).y *40+240)
+    g.moveTo(dotList.at(0).x *40+40,dotList.at(0).y *40+40)
+    g.lineTo(dotList.at(dotList.length-1).x *40+40,dotList.at(dotList.length-1).y *40+40)
     }
   };
   
@@ -103,29 +136,33 @@ const Geometry = ({question ="empty",shape =-1,value =-1,tSettings = -1}) => {
 
   return (
     <div>
-      
+      <Button
+        className={'m-2'}
+        style={{color: 'white'}}
+        variant={"secondary"}
+    >
+        {`${game.question}`}
+    </Button>
+      <div className={`${gameStyles.frame} m-2`}>
+                <GameNav
+                    showPreviousButton={stage !== 0}
+                    handleNextStage={handleNextStage}
+                    
+                />
+                <div className={`w-100 d-flex flex-column align-items-center justify-content-center ${gameStyles.mainContentContainer}`}>
       <Button
       key={69}
       variant={"outline-secondary"}
       
         className={`m-2`}
-        onClick={() =>{
-          setDotList(prevState =>{
-            let newList = []
-            for (let i = 0; i < prevState.length-1; i++){
-              newList.push(prevState.at(i))
-            }
-            return[...newList]
-          })
-        }
-      }
+        onClick={() => deleteLastDot()}
     >
       Odstranit
     </Button>
     
     <Stage id='Stage'
     width={400} 
-    height={600} 
+    height={400} 
     renderOnComponentChange={true}
     raf={false}
     
@@ -138,7 +175,7 @@ const Geometry = ({question ="empty",shape =-1,value =-1,tSettings = -1}) => {
     interactive={true}
     pointerdown={(e) => {
       var x = Math.floor((e.data.global.x-20) / 40)
-      var y = Math.floor((e.data.global.y -220) / 40)
+      var y = Math.floor((e.data.global.y -20) / 40)
       
       
       setDotList(prevState =>{
@@ -169,6 +206,8 @@ const Geometry = ({question ="empty",shape =-1,value =-1,tSettings = -1}) => {
     />
     <Graphics name={"Geometry"} draw={drawGeometry}/>
     </Stage>
+    </div>
+    </div>
     </div>
   );
 }
