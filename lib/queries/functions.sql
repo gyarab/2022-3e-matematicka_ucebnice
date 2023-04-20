95e5add0c762ac6b90cb37e39abb9411d23d932b
jakub.sveda <jakub.sveda@icloud.com>
2023-04-20 15:22:46 +0200

    functions optimization; added function for user verification

--- lib/queries/functions.sql
+++ lib/queries/functions.sql
@@ -14,14 +14,36 @@ as
 $$
 declare
     game_id integer;
-    i       integer := 1;
+    answer  integer;
 begin
     insert into choose_correct_game values (DEFAULT, question, correct_answer, difficulty, length);
     select max(id) into game_id from choose_correct_game;
-    loop
-        insert into choose_correct_game_answers values (DEFAULT, (answers[i]), game_id);
-        i := i + 1;
-        exit when i = array_length(answers, 1) + 1;
-    end loop;
+    foreach answer slice 1 in array answers
+        loop
+            insert into choose_correct_game_answers values (DEFAULT, answer, game_id);
+        end loop;
+end;
+$$;
+
+-- This function verifies whether the user has a valid session or not
+-- returns true if authorized
+-- returns false if not authorized or does not exist
+create function verify_user(userEmail varchar)
+    returns boolean
+    language plpgsql
+as
+$$
+declare
+    expires_time_in_milliseconds bigint;
+begin
+    select max(expires)::bigint
+    into expires_time_in_milliseconds
+    from sessions as ss, users as us where ss.user_id = us.id and us.email = userEmail;
+
+    if expires_time_in_milliseconds is null or expires_time_in_milliseconds <= floor(EXTRACT(epoch FROM now()) * 1000) then
+        return false;
+    else
+        return true;
+    end if;
 end;
 $$;
