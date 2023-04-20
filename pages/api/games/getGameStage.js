b290840ec5662a75341c382acf5b79b4f7e37afe
jakub.sveda <jakub.sveda@icloud.com>
2023-04-20 17:09:09 +0200

    data validation added to getGameStage.js endpoint; requestValidation.js minor updates

--- pages/api/games/getGameStage.js
+++ pages/api/games/getGameStage.js
@@ -1,18 +1,31 @@
 import { isValidRequest} from "../../../lib/utils/requestValidation.js";
 import { gameList } from "../../../lib/database/dbOperations.js";
+import {z} from "zod";
 
 export default async function handler(req, res) {
-    const validation = await isValidRequest(req, 'POST');
+    console.log(req.body)
 
+    const gameIdSchema = z.number().max(1000)
+    const emailSchema = z.string().email().min(5)
+
+    const gameId = gameIdSchema.safeParse(req?.body?.gameId)
+    const email = emailSchema.safeParse(req?.body?.email)
+
+    if (!gameId.success || ! email.success) {
+        return res.status(400).json({
+            err: 'Required body parameters are not valid.'
+        })
+    }
+    console.log(gameId.data, email.data)
+
+    const validation = await isValidRequest(req, email.data, 'POST');
     if (!validation.ok) {
         return res.status(validation.status).json({
             err: validation.message
         })
     }
 
-    const gameId = req.body.gameId
-
-    const stage = gameList.get()
+    const stage = gameList.get(gameId.data)(email.data)
 
     //const content = await gameList.get(1)(1, 3, validation.session)
 
@@ -23,6 +36,6 @@ export default async function handler(req, res) {
 
 
     return res.status(200).json({
-        req: req.body
+
     })
 }
