f442529400c7fb5696f5b1fe1ee4d81b408394c0
jakub.sveda <jakub.sveda@icloud.com>
2023-04-24 20:16:45 +0200

    API endpoint to request equal pair data (here pexeso)

---
+++ pages/api/games/getPexesoGame.js
@@ -0,0 +1,40 @@
+import {isValidRequest} from "../../../lib/utils/requestValidation.js";
+import {getPexesoGame} from "../../../lib/database/dbOperations.js";
+import {difficultySchema, emailSchema, lengthSchema, replacer} from "../../../lib/utils/utils";
+
+export default async function handler(req, res) {
+    //console.log(req.body)
+
+    const email = emailSchema.safeParse(req?.body?.email)
+    const difficulty = difficultySchema.safeParse(req?.body?.difficulty)
+    const size = lengthSchema.safeParse(req?.body?.size)
+
+    if (!email.success || !difficulty.success || !size.success) {
+        return res.status(400).json({
+            err: 'Required body parameters are not valid.'
+        })
+    }
+    // console.log(gameId.data, email.data, difficulty.data, length.data)
+
+    const validation = await isValidRequest(req, email.data, 'POST');
+    if (!validation.ok) {
+        return res.status(validation.status).json({
+            err: validation.message
+        })
+    }
+
+    let pairs = await getPexesoGame(difficulty.data, size.data, email.data)
+    //console.log(stage)
+
+    let response = {
+        ok: false,
+    }
+    if (!(pairs === null || typeof pairs !== 'object')) {
+        response.ok = true
+        response.pairs = JSON.stringify(pairs, replacer)
+    }
+
+    console.log(response.pairs)
+
+    return res.status(200).json(response)
+}
