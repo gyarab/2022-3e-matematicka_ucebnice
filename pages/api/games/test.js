fbd9b0a20aa2635a5f00c3ac21e72bb8a416fc4a
jakub.sveda <jakub.sveda@icloud.com>
2023-04-25 07:55:33 +0200

    test endpoint works for more gameIds

--- pages/api/games/test.js
+++ pages/api/games/test.js
@@ -1,5 +1,5 @@
 import pool from "../../../lib/database/db";
-import {generateEquation, getRandomInt} from "../../../lib/generation/equationGeneration";
+import {generateEquation, generateSorterGameObject, getRandomInt} from "../../../lib/generation/equationGeneration";
 
 export default async function handler(req, res) {
     const client = await pool.connect()
@@ -22,6 +22,8 @@ export default async function handler(req, res) {
             }
             break
         case 2:
+        case 3:
+        case 4:
             for (let i = 0; i < 10; i++) {
                 try {
                     await client.query('BEGIN')
@@ -29,6 +31,23 @@ export default async function handler(req, res) {
                     let a = getRandomInt(0, 10);
                     let b = getRandomInt(0, 10);
                     // `${a * b}`; `${a} * ${b}`
+
+                    await client.query('select set_equal_pair($1, $2, $3);', [1, `${a * b}`, `${a} * ${b}`])
+
+                    await client.query('COMMIT')
+                } catch (e) {
+                    await client.query('ROLLBACK')
+                    console.log(e)
+                    i--
+                }
+            }
+            break
+        case 6:
+            for (let i = 0; i < 10; i++) {
+                try {
+                    await client.query('BEGIN')
+
+                    const sorterGame = generateSorterGameObject(3, 1)
 
                     await client.query('select set_equal_pair($1, $2, $3);', [1, `${a * b}`, `${a} * ${b}`])
 
